fb723cce4038d3a5359a28260990a09c560f3f6e
Vinicius Ferreira <vfbdelima@gmail.com>
2017-09-11 19:04:26 -0300

    Comment routines: application 2
    Signed-off-by: Vinicius Ferreira <vfbdelima@gmail.com>

--- include/JoystickConfigState.h
+++ include/JoystickConfigState.h
@@ -1,5 +1,5 @@
-#ifndef JOYSTICKCONFIGSTATE_H
-#define JOYSTICKCONFIGSTATE_H
+#ifndef INCLUDE_JOYSTICKCONFIGSTATE_H_
+#define INCLUDE_JOYSTICKCONFIGSTATE_H_
 
 #include "State.h"
 #include "Sprite.h"
@@ -8,20 +8,48 @@
 #include "Sound.h"
 
 class JoystickConfigState : public State{
-private:
-	Sprite background, joystick_help, back_btn, test_btn, keyboard;
-	Text *back_txt, *test_txt, *press, *select_text, *and_text, *start_text, *to_go_back;
-	bool on_test, is_keyboard;
-	Sound blocked, selected, changed;
+ private:
+    Sprite background, joystick_help, back_btn, test_btn, keyboard;
+    Text *back_txt, *test_txt, *press, *select_text, *and_text, *start_text,
+        *to_go_back;
+    bool on_test, is_keyboard;
+    Sound blocked, selected, changed;
 
-public:
-	JoystickConfigState(int joystick_id, bool ckeyboard = false);
+ public:
+	 /** Constructor.
+		* This constructor builds sprites to render the option 'JOYSTICK' located on
+		* the menu 'Options' of the game.
+		*
+		* @param joystick_id and integer argument that represents the id of one of
+		* the four joysticks that can be pluged.
+		* @param ckeyboard a boolean argument that indicates if a keyboard is
+		* connected.
+		*/
+    explict JoystickConfigState(int joystick_id, bool ckeyboard = false);
 
-	void update(float delta);
-	void render();
+		/** Function that updates sprites on joystick test mode.
+		 * This function makes sprites change color accoding to the button pressed by
+		 * the user on the joystick test mode located at the menu
+		 * "Options->Joystick->Test".
+		 *
+		 * @param delta a float variation to update joystick state.
+		 */
+    void update(float delta);
+		/** Function that renders keyboard or joystick help images.
+		 * This function renders keyboard or joystick help images if user select one of
+		 * the options: 'keyboard' and 'joystick'; located at the menu 'Options'.
+		 * Besides that, it will render buttons on the joystick test mode.
+		 */
+    void render();
 
-	void pause();
-	void resume();
+		/** Pause function.
+		 * Nothing to do.
+		 */
+    void pause();
+		/** Resume function.
+		 * Nothing to do.
+		 */
+    void resume();
 };
 
-#endif
+#endif  // INCLUDE_JOYSTICKCONFIGSTATE_H_
