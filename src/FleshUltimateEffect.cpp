92524f72b97d24b406b97ada6801c6ac088f6bbc
joaorobson <joaorobsonmartins@gmail.com>
2017-09-11 12:17:52 -0300

    Classes, routines and files comments: Application 1

--- src/FleshUltimateEffect.cpp
+++ src/FleshUltimateEffect.cpp
@@ -1,5 +1,23 @@
+/** Copyright (c) 2017 Wenova - Rise of Conquerors. All rights reserved.
+*
+* This work is licensed under the terms of the MIT license.
+
+* For a copy, see <https://opensource.org/licenses/MIT>.
+*
+* This file contains the definition of the FleshUltimateEffect class methods.
+*/
+
 #include "FleshUltimateEffect.h"
 
+/** The constructor.
+* Initialize the fighter character and the respective sprite, tags and frames
+* of the FleshUltimateEffect.
+*
+* @param cparent is a Fighter object.
+* @param csprite is the respective sprite.
+* @param ctags is the character condition tag.
+* @param frames is the respective effect frame.
+*/
 FleshUltimateEffect::FleshUltimateEffect(Fighter * cparent,
                                          string csprite,
                                          string ctags,
@@ -12,6 +30,11 @@ FleshUltimateEffect::FleshUltimateEffect(Fighter * cparent,
   parent->MAX_LIFE *= 1.5;
 }
 
+/** Update effect position.
+* Update effect position according to character old position.
+*
+* @param delta is the character state variation.
+*/
 void FleshUltimateEffect::update(float delta) {
   if (parent) {
     box.x = parent->box.x;
@@ -20,10 +43,19 @@ void FleshUltimateEffect::update(float delta) {
   }
   sprite.update(delta);
 }
+
+/** Render method.
+* Show effect on screen according to character current position.
+*/
 void FleshUltimateEffect::render() {
   sprite.render(box.get_draw_x(), box.get_draw_y(), rotation);
 }
 
+/** Check life condition method.
+* Check if the character is dead and change his tags.
+*
+*@return Return true if the character is dead and false if not.
+*/
 bool FleshUltimateEffect::is_dead() {
   bool dead = parent->get_special() <= 0 or parent->is("dying");
   if (dead) {
