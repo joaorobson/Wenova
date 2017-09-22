e2f8c4638be2563abc1bb7824d8b33ec1dc7b522
joaorobson <joaorobsonmartins@gmail.com>
2017-09-22 10:58:29 -0300

    Little repairs in doxygen comments style

--- src/FleshUltimateEffect.cpp
+++ src/FleshUltimateEffect.cpp
@@ -1,26 +1,26 @@
 /* Copyright (c) 2017 Wenova - Rise of Conquerors. All rights reserved.
-*
-* This work is licensed under the terms of the MIT license.
-
-* For a copy, see <https://opensource.org/licenses/MIT>.
-*/
+ *
+ * This work is licensed under the terms of the MIT license.
+ *
+ * For a copy, see <https://opensource.org/licenses/MIT>.
+ */
 /**
-* @file FleshUltimateEffect.cpp
-* This file contains the definition of the FleshUltimateEffect class methods.
-*/
+ * @file FleshUltimateEffect.cpp
+ * This file contains the definition of the FleshUltimateEffect class methods.
+ */
 
 #include "FleshUltimateEffect.h"
 
 /**
-* The constructor.
-* Initialize the fighter character and the respective sprite, tags and frames
-* of the FleshUltimateEffect.
-*
-* @param cparent is a Fighter object.
-* @param csprite is the respective sprite.
-* @param ctags is the character condition tag.
-* @param frames is the respective effect frame.
-*/
+ * The constructor.
+ * Initialize the fighter character and the respective sprite, tags and frames
+ * of the FleshUltimateEffect.
+ *
+ * @param cparent is a Fighter object.
+ * @param csprite is the respective sprite.
+ * @param ctags is the character condition tag.
+ * @param frames is the respective effect frame.
+ f*/
 FleshUltimateEffect::FleshUltimateEffect(Fighter * cparent,
                                          string csprite,
                                          string ctags,
@@ -34,11 +34,11 @@ FleshUltimateEffect::FleshUltimateEffect(Fighter * cparent,
 }
 
 /**
-* Update effect position.
-* Update effect position according to character old position.
-*
-* @param delta is the character state variation.
-*/
+ * Update effect position.
+ * Update effect position according to character old position.
+ *
+ * @param delta is the character state variation.
+ */
 void FleshUltimateEffect::update(float delta) {
   if (parent) {
     box.x = parent->box.x;
@@ -49,19 +49,19 @@ void FleshUltimateEffect::update(float delta) {
 }
 
 /**
-* Render method.
-* Show effect on screen according to character current position.
-*/
+ * Render method.
+ * Show effect on screen according to character current position.
+ */
 void FleshUltimateEffect::render() {
   sprite.render(box.get_draw_x(), box.get_draw_y(), rotation);
 }
 
 /**
-* Check life condition method.
-* Check if the character is dead and change his tags.
-*
-*@return Return true if the character is dead and false if not.
-*/
+ * Check life condition method.
+ * Check if the character is dead and change his tags.
+ *
+ *@return Return true if the character is dead and false if not.
+ */
 bool FleshUltimateEffect::is_dead() {
   bool dead = parent->get_special() <= 0 or parent->is("dying");
   if (dead) {
