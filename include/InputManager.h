ea5f745312b5ee82b2f9b84915e19c65353916fa
Ícaro Pires <icaropiresdf@gmail.com>
2017-10-08 20:18:40 -0300

    Apply constant technique, adapt comments  and gets much better variables
    Signed-off-by: Ícaro Pires <icaropiresdf@gmail.com>

--- include/InputManager.h
+++ include/InputManager.h
@@ -20,6 +20,10 @@
 
 #define ii std::pair<int, int>
 
+#define N_CONTROLLERS 4
+#define N_MOUSE_BUTTONS 6
+#define N_JOYSTICK_BUTTONS 10
+
 class InputManager {
  private:
     static InputManager* input_manager;
@@ -27,11 +31,11 @@ class InputManager {
     /**
      * For mouse is not unordered_map because there is only one mouse.
      */
-    bool mouse_buttons_states[6];
-    int mouse_update[6];
+    bool mouse_buttons_states[N_MOUSE_BUTTONS];
+    int mouse_update[N_MOUSE_BUTTONS];
 
-    std::unordered_map<int, bool> joysticks_buttons_states[10];
-    std::unordered_map<int, int> joystick_update[10];
+    std::unordered_map<int, bool> joysticks_buttons_states[N_JOYSTICK_BUTTONS];
+    std::unordered_map<int, int> joystick_update[N_JOYSTICK_BUTTONS];
 
     std::unordered_map<int, bool> keys_states;
     std::unordered_map<int, int> keys_updates;
@@ -41,11 +45,11 @@ class InputManager {
 
     std::unordered_map<int, int> keyboard_to_joystick;
 
-    SDL_GameController* controllers[4];
+    SDL_GameController* controllers[N_CONTROLLERS];
 
     bool has_quit_request;
     int update_counter;  ///< Will be incrementes when updating input state,
-                         ///< refer to key presses.
+    ///< refer to key presses.
 
     int mouse_x_position; /**< Unit: px */
     int mouse_y_position; /**< Unit: px */
@@ -54,17 +58,17 @@ class InputManager {
      * Variations in mouse position in each axis.
      * Unit: px;
      */
-    int mouse_delta_x; /**< [0, 1280] */
-    int mouse_delta_y; /**< [0, 720] */
+    int offset_x; /**< Unit: px, [0, 1280] */
+    int offset_y; /**< Unit: px, [0, 720] */
 
     int keyboard_to_joystick_id;  ///< Which profile will be used for mapping
-                                  ///< keyboard_for_joystick.
-                                  ///< Ex: (Menu handler or gameplay).
+    ///< keyboard_for_joystick.
+    ///< Ex: (Menu handler or gameplay).
 
     float mouse_sensibility_value; /**< Refer to mouse sensibility. */
-    int analogic_sensibility_value = 20000; /**< Refer to joystick lever sensibility. */
-    int triggers_sensibility_value =
-        32000; /**< Refer to joystick RT and LT (triggers) lever sensibility. */
+    int analogic_sensibility_value; /**< Refer to joystick lever sensibility.*/
+    int triggers_sensibility_value;  ///< Refer to joystick RT and LT
+    ///< (triggers) lever sensibility. */
 
     /**
      * Manages joystick interaction with the game.
@@ -208,13 +212,15 @@ class InputManager {
 
     /**
      * Configure mouse scale to calibrae sensibility.
-     * Bigger the values, more sensible the mouse will be
+     * Bigger the values, more sensible the mouse will be. Util for when
+     * resizing the screen
      *
      * @param cmouse_sensibility_value Unit: px, [0,]
-     * @param cmouse_delta_x Unit: px, [0,]
-     * @param cmouse_delta_y Unit: px, [0,]
+     * @param coffset_x Unit: px, [0,]
+     * @param coffset_y Unit: px, [0,]
      */
-    void set_mouse_sensibility_value(float mouse_sensibility_value, int mouse_delta_x, int mouse_delta_y);
+    void set_mouse_sensibility_value(float mouse_sensibility_value,
+                                     int offset_x, int offset_y);
 
     /**
      * Set value for joystick hand crank.
@@ -235,73 +241,85 @@ class InputManager {
      */
     void map_keyboard_to_joystick(int map_id = 0);
 
-    // joystick keys
-    static const int UP = SDL_CONTROLLER_BUTTON_DPAD_UP;
-    static const int RIGHT = SDL_CONTROLLER_BUTTON_DPAD_RIGHT;
-    static const int DOWN = SDL_CONTROLLER_BUTTON_DPAD_DOWN;
-    static const int LEFT = SDL_CONTROLLER_BUTTON_DPAD_LEFT;
-    static const int A = SDL_CONTROLLER_BUTTON_A;
-    static const int B = SDL_CONTROLLER_BUTTON_B;
-    static const int X = SDL_CONTROLLER_BUTTON_X;
-    static const int Y = SDL_CONTROLLER_BUTTON_Y;
-    static const int LB = SDL_CONTROLLER_BUTTON_LEFTSHOULDER;
-    static const int RB = SDL_CONTROLLER_BUTTON_RIGHTSHOULDER;
-    static const int LT = 0x20;
-    static const int RT = 0x21;  // Valor arbitrário, mas tá certo
-    static const int SELECT = SDL_CONTROLLER_BUTTON_BACK;
-    static const int START = SDL_CONTROLLER_BUTTON_START;
-    static const int L3 = SDL_CONTROLLER_BUTTON_LEFTSTICK;
-    static const int R3 = SDL_CONTROLLER_BUTTON_RIGHTSTICK;
+    /**
+     * Joysitck keys.
+     */
+    static const int UP = SDL_CONTROLLER_BUTTON_DPAD_UP; /**< Same as w. */
+    static const int RIGHT = SDL_CONTROLLER_BUTTON_DPAD_RIGHT; /**< Same as d.*/
+    static const int DOWN = SDL_CONTROLLER_BUTTON_DPAD_DOWN; /**< Same as s. */
+    static const int LEFT = SDL_CONTROLLER_BUTTON_DPAD_LEFT; /**< Same a. */
+    static const int A = SDL_CONTROLLER_BUTTON_A; /**< Same as space. */
+    static const int B = SDL_CONTROLLER_BUTTON_B; /**< Same as k. */
+    static const int X = SDL_CONTROLLER_BUTTON_X; /**< Same h. */
+    static const int Y = SDL_CONTROLLER_BUTTON_Y; /**< Same r. */
+    static const int LB = SDL_CONTROLLER_BUTTON_LEFTSHOULDER; /**< Same as j. */
+    static const int RB = SDL_CONTROLLER_BUTTON_RIGHTSHOULDER; /**< Same as l */
+    static const int RT = 0x21;  ///< Arbitrary value, but it's right.
+    ///< Same as .(period).
+    static const int LT = 0x20; /**< Same as , (comma) */
+    static const int SELECT = SDL_CONTROLLER_BUTTON_BACK; /**< Same as esc. */
+    static const int START = SDL_CONTROLLER_BUTTON_START; /**< Same as enter. */
+    static const int L3 = SDL_CONTROLLER_BUTTON_LEFTSTICK; /**< Same as LT. */
+    static const int R3 = SDL_CONTROLLER_BUTTON_RIGHTSTICK; /**< Same as RB. */
 
-    // keys for battle
-    static const int K_UP = SDLK_w;
-    static const int K_RIGHT = SDLK_d;
-    static const int K_DOWN = SDLK_s;
-    static const int K_LEFT = SDLK_a;
-    static const int K_A = SDLK_SPACE;  // jump
-    static const int K_B = SDLK_k;  // skill 1
-    static const int K_X = SDLK_h;  // attack
-    static const int K_Y = SDLK_r;  // ultimate
-    static const int K_LB = SDLK_j;  // defense
-    static const int K_RB = SDLK_l;  // skill 2
-    static const int K_LT = SDLK_COMMA;  // change skin left
-    static const int K_RT = SDLK_PERIOD;  // change skin right
-    static const int K_SELECT = SDLK_ESCAPE;  // cancel / back
-    static const int K_START = SDLK_RETURN;  // confirm / start
-    static const int ENTER_KEY = SDLK_RETURN;  // confirm / start
-    static const int K_RANDOM = SDLK_u;  // random
-    static const int RIGHT_MOUSE_BUTTON = SDL_BUTTON_RIGHT;
-    static const int LEFT_MOUSE_BUTTON = SDL_BUTTON_LEFT;
-    static const int K_L3 = SDLK_COMMA;
-    static const int K_R3 = SDLK_PERIOD;
+    /**
+     * Keys for battle.
+     */
+    static const int K_UP = SDLK_w; /**< Nothing. */
+    static const int K_RIGHT = SDLK_d; /**< Move right. */
+    static const int K_DOWN = SDLK_s; /**< Crouch. */
+    static const int K_LEFT = SDLK_a; /**< Move left. */
+    static const int K_A = SDLK_SPACE; /**< Jump. */
+    static const int K_B = SDLK_k; /**< Skill 1 */
+    static const int K_X = SDLK_h; /**< Attack. */
+    static const int K_Y = SDLK_r; /**< Ultimate. */
+    static const int K_LB = SDLK_j; /**< Defense. */
+    static const int K_RB = SDLK_l; /**< Skill 2 */
+    static const int K_LT = SDLK_COMMA; /**< Change skin left. */
+    static const int K_RT = SDLK_PERIOD; /**< Change skin right. */
+    static const int K_SELECT = SDLK_ESCAPE; /**< Cancel / back. */
+    static const int K_START = SDLK_RETURN; /**< Confirm / start. */
+    static const int ENTER_KEY = SDLK_RETURN; /**< Confirm / start. */
+    static const int K_RANDOM = SDLK_u; /**< Random. */
 
-    // keys for menus
-    static const int K_MENU_A = SDLK_h;
-    static const int K_MENU_B = SDLK_j;
-    static const int K_MENU_Y = SDLK_u;
-    static const int K_MENU_LB = SDLK_y;
+    /**
+     * Keys for menus.
+     */
+    static const int K_MENU_A = SDLK_h; /**< Confirm. */
+    static const int K_MENU_B = SDLK_j; /**< Go back. */
+    static const int K_MENU_Y = SDLK_u; /**< Random. */
+    static const int K_MENU_LB = SDLK_y; /**< Nothing. */
+    static const int K_L3 = SDLK_COMMA; /**< Change skin left. */
+    static const int K_R3 = SDLK_PERIOD; /**< Change skin right. */
 
-    // keys for edit mode
-    static const int K_ARROW_UP = SDLK_UP;
-    static const int K_ARROW_RIGHT = SDLK_RIGHT;
-    static const int K_ARROW_DOWN = SDLK_DOWN;
-    static const int K_ARROW_LEFT = SDLK_LEFT;
-    static const int K_CTRL = SDLK_LCTRL;
-    static const int K_C = SDLK_c;
-    static const int K_S = SDLK_s;
-    static const int K_F = SDLK_f;
-    static const int K_P = SDLK_p;
-    static const int K_O = SDLK_o;
-    static const int K_SHIFT = SDLK_LSHIFT;
-    static const int K_ROT_LEFT = SDLK_z;
-    static const int K_ROT_RIGHT = SDLK_x;
-    static const int K_ROT_RESET = SDLK_r;
-    static const int K_INC_W = SDLK_PERIOD;
-    static const int K_DEC_W = SDLK_COMMA;
-    static const int K_DEL = SDLK_DELETE;
+    /**
+     * Keys for edit mode.
+     */
+    static const int K_ARROW_UP = SDLK_UP; /**< Move platform up. */
+    static const int K_ARROW_RIGHT = SDLK_RIGHT; /**< Move platform right. */
+    static const int K_ARROW_DOWN = SDLK_DOWN; /**< Move platform down. */
+    static const int K_ARROW_LEFT = SDLK_LEFT; /**< Move platofrm left. */
+    static const int K_CTRL = SDLK_LCTRL; /**< Nothing. */
+    static const int K_C = SDLK_c; /**< Make platform rigid / crossingable.  */
+    static const int K_S = SDLK_s;  ///< Cross the platform
+                                    ///< allowed if pressed twice.
+    static const int K_F = SDLK_f; /**< Put rigid platform. */
+    static const int K_P = SDLK_p; /**< Put crossingable platform. */
+    static const int K_O = SDLK_o; /**< Nothing. */
+    static const int K_SHIFT = SDLK_LSHIFT; /**< Change character. */
+    static const int K_ROT_LEFT = SDLK_z; /**< Rotate left. */
+    static const int K_ROT_RIGHT = SDLK_x; /**< Rotate right. */
+    static const int K_ROT_RESET = SDLK_r; /**< Remove rotating. */
+    static const int K_INC_W = SDLK_PERIOD; /**< Increase platform size. */
+    static const int K_DEC_W = SDLK_COMMA; /**< Decrease platform size. */
+    static const int K_DEL = SDLK_DELETE; /**< Delete platform. */
+    static const int RIGHT_MOUSE_BUTTON = SDL_BUTTON_RIGHT;  ///< Bring char
+    ///< to cursor.
+    static const int LEFT_MOUSE_BUTTON =
+        SDL_BUTTON_LEFT; /**< Select platform. */
 
-    static const int MENU_MODE = 0;
-    static const int BATTLE_MODE = 1;
+    static const int MENU_MODE = 0; /**< Set the commands to menu. */
+    static const int BATTLE_MODE = 1; /**< Set the commands to battle. */
 };
 
 #endif  // INCLUDE_INPUTMANAGER_H_
