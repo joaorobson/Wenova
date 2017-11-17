9f7f420879ccc6470661d28fa9015add82d08030
rafaelbrg <rabragancaunb@gmail.com>
2017-11-17 03:40:15 -0200

    Uso de constantes 1

--- src/MenuState.cpp
+++ src/MenuState.cpp
@@ -31,6 +31,21 @@
 #define FRAME_TIME 7.5
 #define TEXT_TIMER_COOLDOWN 50
 
+#define N_ZERO 0
+#define N_ONE 1
+#define N_TWO 2
+
+#define N_PLANET_RENDER_1 423
+#define N_PLANET_RENDER_2 177
+#define N_GREENSHIP_RENDER_1 805
+#define N_GREENSHIP_RENDER_2 405
+#define N_REDSHIP_RENDER_1 36
+#define N_REDSHIP_RENDER_2 400
+#define N_TITLE_RENDER 260
+
+#define N_CHANNELS 50
+#define FONT_SIZE 30
+#define SENSIBILITY_VALUE 32000
 /**
  * The constructor.
  * Initialize the game menu.
@@ -38,40 +53,41 @@
  * @param main_menu is a boolean type that send true or false to the method.
  */
 MenuState::MenuState(bool main_menu) {
-    Mix_AllocateChannels(50);
+    Mix_AllocateChannels(N_CHANNELS);
 
-    current_option = 0;
+    current_option = N_ZERO;
     start_pressed  = main_menu;
     show_text      = true;
 
     background = Sprite("menu/background.jpg");
     title      = Sprite("menu/title.png", 5, FRAME_TIME);
     planet     = Sprite("menu/planet.png", 8, FRAME_TIME);
-    green_ship = Sprite("menu/green_ship.png", 8, FRAME_TIME, 0, 2);
+    green_ship = Sprite("menu/green_ship.png", 8, FRAME_TIME, N_ZERO, 2);
     red_ship   = Sprite("menu/red_ship.png", 8, FRAME_TIME);
 
     start_option = new Text("font/8-BIT WONDER.ttf",
-                            30,
+                            FONT_SIZE,
                             Text::TextStyle::SOLID,
                             "PRESS START",
                             LIGHT_GREEN,
                             FONT_X,
                             FONT_Y);
 
-    options.push_back(new Text("font/8-BIT WONDER.ttf", 30,
+    options.push_back(new Text("font/8-BIT WONDER.ttf", FONT_SIZE,
                                Text::TextStyle::SOLID, "START", WHITE, FONT_X,
                                FONT_Y));
-    options.push_back(new Text("font/8-BIT WONDER.ttf", 30,
+    options.push_back(new Text("font/8-BIT WONDER.ttf", FONT_SIZE,
                                Text::TextStyle::SOLID, "EDIT", WHITE, FONT_X,
                                FONT_Y));
-    options.push_back(new Text("font/8-BIT WONDER.ttf", 30,
+    options.push_back(new Text("font/8-BIT WONDER.ttf", FONT_SIZE,
                                Text::TextStyle::SOLID, "OPTIONS", WHITE, FONT_X,
                                FONT_Y));
-    options.push_back(new Text("font/8-BIT WONDER.ttf", 30,
+    options.push_back(new Text("font/8-BIT WONDER.ttf", FONT_SIZE,
                                Text::TextStyle::SOLID, "EXIT", WHITE, FONT_X,
                                FONT_Y));
 
-    InputManager::get_instance()->set_analogic_sensibility_value(32000);
+    InputManager::get_instance()->
+                              set_analogic_sensibility_value(SENSIBILITY_VALUE);
     InputManager::get_instance()->
 															map_keyboard_to_joystick(InputManager::MENU_MODE);
 
@@ -80,6 +96,7 @@ MenuState::MenuState(bool main_menu) {
     changed  = Sound("menu/sound/cursor.ogg");
 
     music = Music("menu/wenova.ogg");
+
     /**
      * Plays music if it is not playing.
      */
@@ -122,7 +139,7 @@ void MenuState::update(float delta) {
     /**
      * Select options in the menu.
      */
-    if (pressed[LEFT] && (current_option != 0)) {
+    if (pressed[LEFT] && (current_option != N_ZERO)) {
         changed.play();
         current_option--;
     } else {
@@ -158,18 +175,18 @@ void MenuState::update(float delta) {
 
         if (not start_pressed) {
             start_pressed  = true;
-            current_option = 0;
+            current_option = N_ZERO;
         } else {
             m_quit_requested = true;
 
             /**
              * Opens selected option depending on selected index.
              */
-            if (current_option == 0) {
+            if (current_option == N_ZERO) {
                 Game::get_instance().push(new StageSelectState());
-            } else if (current_option == 1) {
+            } else if (current_option == N_ONE) {
                 Game::get_instance().push(new StageSelectState(true));
-            } else if (current_option == 2) {
+            } else if (current_option == N_TWO) {
                 Game::get_instance().push(new OptionsState());
             } else {
               /*Nothing to do*/
@@ -200,7 +217,7 @@ void MenuState::update(float delta) {
         /**
          * Modify the options positioning.
          */
-        for (int idx = 0; idx < current_option; idx++) {
+        for (int idx = N_ZERO; idx < current_option; idx++) {
             Text *next_option = options[idx + 1];
 
             int new_x = next_option->get_x() - options[idx]->get_width() -
@@ -242,22 +259,22 @@ void MenuState::update(float delta) {
  * this method renders the menu of the game on the screen.
  */
 void MenuState::render() {
-    background.render(0, 0);
+    background.render(N_ZERO, N_ZERO);
 
-    planet.render(423, 177);
-    green_ship.render(805, 405);
-    red_ship.render(36, 400);
-    title.render(260, 0);
+    planet.render(N_PLANET_RENDER_1, N_PLANET_RENDER_2);
+    green_ship.render(N_GREENSHIP_RENDER_1, N_GREENSHIP_RENDER_2);
+    red_ship.render(N_REDSHIP_RENDER_1, N_REDSHIP_RENDER_2);
+    title.render(N_TITLE_RENDER, N_ZERO);
 
     /**
      * Renders all the options on screen.
      */
     if (start_pressed) {
         for (auto option_text : options) {
-            option_text->render(0, 0);
+            option_text->render(N_ZERO, N_ZERO);
         }
     } else if (show_text) {
-        start_option->render(0, 0);
+        start_option->render(N_ZERO, N_ZERO);
     } else {
       /*Nothing to do*/
     }
@@ -289,10 +306,10 @@ void MenuState::process_input() {
     for (ii button : joystick_buttons) {
         pressed[button.first]    = input_manager->joystick_button_press(
             button.second,
-            0);
+            N_ZERO);
         is_holding[button.first] = input_manager->is_joystick_button_down(
             button.second,
-            0);
+            N_ZERO);
     }
 }
 
